5df88dcb5f03f4c34077a0fb333bb7eaf8a275c0
yanarepak <112949438+yanarepak@users.noreply.github.com>
2022-12-20 00:31:46 +0200

    Починає 1 та 2 задачу

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryEl = document.querySelector(".gallery");
+
+
+const createGallery =  galleryItems
+.map(({preview, original, description}) => 
+{
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+})
+.join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", createGallery);
+
+const openImg = event => {
+    event.preventDefault();
+    if(event.target.nodeName !== "IMG"){
+        return
+    }
+    
+    const instance = basicLightbox.create(`
+<img scr =${event.target.dataset.source} width="800" height="600">`);
+    instance.show();
+
+    galleryEl.addEventListener("keydown", event => {
+      if(event.code === "Escape"){
+      instance.close();
+    }
+    });
+}
+
+galleryEl.addEventListener("click", openImg);
 console.log(galleryItems);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const createGallery = galleryItems
+.map(({preview, original, description}) => 
+{
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+  </a>`;
+})
+.join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", createGallery);
 
 console.log(galleryItems);
